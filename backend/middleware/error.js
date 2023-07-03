74f9645c6efb36e4569357bb8f2a4ad4f4b39b4a
Alphaviper7769 <aayushraturi@gmail.com>
2023-07-03 20:35:59 +0530

    auth finished+ error handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,9 +10,30 @@ export default (err,req,res,next) => {
         err = new ErrorHandler(message,400);
     }
 
+    //Mongooose duplicate key error
+    if(err.code === 11000)
+    {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`
+        err = new ErrorHandler(message,400);
+    }
+
+    //Wrong JWT error
+    if(err.name === "JsonWebTokenError") {
+        const message = `JSON Web Token invalid, try again`
+        err = new ErrorHandler(message,400);
+    }
+
+     //JWT Expire Error
+     if(err.name === "TokenExpiredError") {
+        const message = `JSON Web Token is Expired, try again`
+        err = new ErrorHandler(message,400);
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message:err.message,
         error:err.stack
     })
 }
+
+
