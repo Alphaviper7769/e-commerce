f1eba287fff1be3eb6b37433060fe3e72abe418c
Alphaviper7769 <aayushraturi@gmail.com>
2023-07-03 20:22:36 +0530

    authentication over

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,10 +4,12 @@ import catchAsyncError from '../middleware/catchAsyncError.js'
 export const sendEmail = catchAsyncError( async (options) => {
 
     const transporter = nodeMailer.createTransport({
+        host:process.env.SMPT_HOST,
+        port:process.env.SMPT_PORT,
         service:process.env.SMPT_SERVICE,
         auth:{
             user:process.env.SMPT_MAIL,
-            password:process.env.SMPT_PASSWORD,
+            pass:process.env.SMPT_PASSWORD,
         }
     })
 
