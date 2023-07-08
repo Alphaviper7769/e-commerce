689f19dc7b77a249b342f7e524d4a83ba8a18681
Alphaviper7769 <aayushraturi@gmail.com>
2023-07-08 16:52:33 +0530

    initialize

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,9 @@
-import express from "express"
-import ErrorHandler from "./utils/errorHandler.js";
-import errorMiddleware from "./middleware/error.js"
-import cookieParser from "cookie-parser";
-
-const app = express();
-app.use(express.json())
-app.use(cookieParser())
-
-//routes import
-import product from "./routes/productRoute.js"
-import user from "./routes/userRoute.js"
-
-
-app.use("/api/v1",product)
-app.use("/api/v1",user)
+const app = require("./app");
 
 
 
-//Middleware for Error
-app.use(errorMiddleware)
 
+app.listen(process.env.PORT,()=>{
 
-export default app;
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+})
--- backend/middleware/catchAsyncError.js
+++
@@ -1,5 +0,0 @@
-
-export default (asyncError) => (req,res,next) => {
-
-    Promise.resolve(asyncError(req,res,next)).catch(next);
-}
